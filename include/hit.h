69cf36d692818f63e1841b0500d4007b68ac535e
Ana <anaigd93@gmail.com>
2025-10-23 13:13:25 +0200

    ADD: cylinder

--- include/hit.h
+++ include/hit.h
@@ -12,7 +12,7 @@
 typedef struct s_hit
 {
 	int		ok;
-	float		t; // Distancia al primer objeto intersectado
+	float	t; // Distancia al primer objeto intersectado
 	t_vec3	p; // Punto de impacto
 	t_vec3	n; // Normal en el punto
 	t_vec3	albedo; // Color del objeto intersectado
@@ -33,6 +33,6 @@ int		scene_hit(const t_scene *scene, t_ray r, float max_dist, t_hit *out);
 /* HIT OBJECTS */
 float	hit_sphere(const t_sphere *sp, t_ray r);
 float	hit_plane(const t_plane *pl, t_ray r);
-float	hit_cylinder(const t_cyl *cy, t_ray r);
+float	hit_cylinder(const t_cyl *cy, t_ray r, int *hit_part);
 
 #endif
