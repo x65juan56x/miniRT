252ea8488c5e1c9de6ae2901fa1f307aeca41aad
Ana <anaigd93@gmail.com>
2025-11-02 12:59:54 +0100

    minirt.c refactorizado

--- src/minirt.c
+++ src/minirt.c
@@ -6,13 +6,13 @@
 #include "../include/shading.h"
 #include "../include/app.h"
 
-static void render_and_present(t_app *app)
+static void	render_and_present(t_app *app)
 {
 	render_scene(app);
 	upload_framebuffer(app->image, app->framebuffer);
 }
 
-static int init_window(t_app *app)
+static int	init_window(t_app *app)
 {
 	app->mlx = mlx_init(WIN_W, WIN_H, "miniRT", false);
 	if (!app->mlx)
@@ -34,7 +34,7 @@ static int init_window(t_app *app)
 	return (0);
 }
 
-static void cleanup(t_app *app)
+static void	cleanup(t_app *app)
 {
 	if (app->overlay.overlay && app->mlx)
 		mlx_delete_image(app->mlx, app->overlay.overlay);
@@ -51,7 +51,89 @@ static void cleanup(t_app *app)
 	free(app->framebuffer);
 }
 
+static int print_usage(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		ft_putstr_fd((char *)"Usage: ", 2);
+		if (av[0])
+			ft_putstr_fd(av[0], 2);
+		ft_putstr_fd((char *)" <scene.rt>\n", 2);
+		return (1);
+	}
+	return (0);
+}
+
+static int init_framebuffer(t_app *app)
+{
+	app->framebuffer = (uint32_t *)malloc(sizeof(uint32_t)
+			*(size_t)WIN_W * (size_t)WIN_H);
+	if (!app->framebuffer)
+	{
+		ft_putstr_fd((char *)"Error\nfailed to allocate framebuffer\n", 2);
+		scene_free(&app->scene);
+		return (0);
+	}
+	return (1);
+}
+
+t_parse_result init_and_parse(t_app *app, char **av)
+{
+	t_parse_result	pr;
+	ft_bzero(app, sizeof(app));
+	scene_init(&app->scene);
+	pr = parse_scene(av[1], &app->scene);
+	if (!pr.ok)
+	{
+		ft_putstr_fd((char *)"Error\n", 2);
+		if (pr.message)
+		{
+			ft_putstr_fd(pr.message, 2);
+			ft_putstr_fd((char *)"\n", 2);
+		}
+		else
+			ft_putstr_fd((char *)"parse failed\n", 2);
+		parse_result_free(&pr);
+		scene_free(&app->scene);
+	}
+	return (pr);
+}
+
 int	main(int ac, char **av)
+{
+	t_app			app;
+	t_parse_result	pr;
+
+	if (print_usage(ac, av))
+		return (1); 
+	pr = init_and_parse(&app, av);
+	if(!pr.ok)
+		return (EXIT_FAILURE);
+	parse_result_free(&pr);
+	if (!init_framebuffer(&app))
+		return(EXIT_FAILURE);
+/* 	app.framebuffer = (uint32_t *)malloc(sizeof(uint32_t)
+			*(size_t)WIN_W * (size_t)WIN_H);
+	if (!app.framebuffer)
+	{
+		ft_putstr_fd((char *)"Error\nfailed to allocate framebuffer\n", 2);
+		scene_free(&app.scene);
+		return (1);
+	} */
+	if (init_window(&app) < 0)
+	{
+		cleanup(&app);
+		return (1);
+	}
+	ti_init(&app.overlay, app.mlx, app.image);
+	app.show_normals = 0;
+	render_and_present(&app);
+	mlx_key_hook(app.mlx, &app_on_key, &app);
+	mlx_loop(app.mlx);
+	cleanup(&app);
+	return (0);
+}
+/* int	main(int ac, char **av)
 {
 	t_app			app;
 	t_parse_result	pr;
@@ -82,7 +164,8 @@ int	main(int ac, char **av)
 		return (EXIT_FAILURE);
 	}
 	parse_result_free(&pr);
-	app.framebuffer = (uint32_t *)malloc(sizeof(uint32_t) * (size_t)WIN_W * (size_t)WIN_H);
+	app.framebuffer = (uint32_t *)malloc(sizeof(uint32_t)
+			*(size_t)WIN_W * (size_t)WIN_H);
 	if (!app.framebuffer)
 	{
 		ft_putstr_fd((char *)"Error\nfailed to allocate framebuffer\n", 2);
@@ -102,3 +185,4 @@ int	main(int ac, char **av)
 	cleanup(&app);
 	return (0);
 }
+ */
