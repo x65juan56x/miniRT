099aa044b10c6d9161f960af9913bcf3a9834615
Ana Garrido Diaz <anagarri@c1r5s3.42malaga.com>
2025-11-03 16:28:31 +0100

    math_utils.c + render_bonus.c

--- include_bonus/math_utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATH_UTILS_BONUS_H
-# define MATH_UTILS_BONUS_H
-
-# include "../include/math_utils.h"
-
-#endif
--- include_bonus/render_bonus.h
+++ include_bonus/render_bonus.h
@@ -1,13 +1,13 @@
 #ifndef RENDER_BONUS_H
-# define  RENDER_BONUS_H
+# define RENDER_BONUS_H
 
 # include <stdint.h>
 # include "minirt_bonus.h"
 # include "camera_bonus.h"
 
 // Forward declaration to avoid pulling app/scene into this public header
-struct s_app;
-typedef struct s_app t_app;
+struct					s_app;
+typedef struct s_app	t_app;
 
 typedef struct s_render_aux
 {
