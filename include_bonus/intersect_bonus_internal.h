c593bf3d0564f716d5b8861dc8fc1cef5b8860fe
Ana Garrido Diaz <anagarri@c1r5s3.42malaga.com>
2025-11-03 16:14:11 +0100

    intersect_bonus_internal.h

--- include_bonus/intersect_bonus_internal.h
+++ include_bonus/intersect_bonus_internal.h
@@ -1,5 +1,5 @@
 #ifndef INTERSECT_BONUS_INTERNAL_H
-#define INTERSECT_BONUS_INTERNAL_H
+# define INTERSECT_BONUS_INTERNAL_H
 
 # include "minirt_bonus.h"
 
@@ -9,19 +9,19 @@ void	orient_normal(t_hit *hit, t_ray r);
 void	apply_specular(t_hit *out, const t_material *material);
 
 /*intersect_sphere_bonus.c */
-int 	record_sphere(t_sphere *sp, t_ray r, float t, t_hit *out);
+int		record_sphere(t_sphere *sp, t_ray r, float t, t_hit *out);
 
 /* intersect_plane_bonus.c */
-int 	record_plane(t_plane *pl, t_ray r, float t, t_hit *out);
+int		record_plane(t_plane *pl, t_ray r, float t, t_hit *out);
 
 /* intersect_triangle_bonus.c */
-int 	record_triangle(t_triangle *tr, t_ray r, float t, t_hit *out);
+int		record_triangle(t_triangle *tr, t_ray r, float t, t_hit *out);
 
 /* intersect_hparaboloid_bonus.c */
-int	    record_hparaboloid(const t_hparab *hp, t_ray r, float t, t_hit *out);
+int		record_hparaboloid(const t_hparab *hp, t_ray r, float t, t_hit *out);
 
 /* src_bonus/core/intersect_cylinder_bonus.c */
-int	    record_cylinder(t_cyl *cy, t_ray r, float t, t_hit *out);
+int		record_cylinder(t_cyl *cy, t_ray r, float t, t_hit *out);
 void	cy_hit_wall(t_cyl *cy, t_common_hit *c_hit, t_hit *out);
 void	cy_hit_top(t_cyl *cy, t_common_hit *c_hit, t_hit *out);
 void	cy_hit_bottom(t_cyl *cy, t_common_hit *c_hit, t_hit *out);
